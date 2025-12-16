cd7421b03b93c13f776097d001f6841f676b57f7
FlexXzito <davidfe0545@gmail.com>
2025-12-15 22:15:08 -0500

    Update 1.0.1 File Error 2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -64,20 +64,21 @@ const About = () => {
         </div>
         <div className="mt-10 flex justify-center">
           <a
-            href="../../public/CV_HV DavidFVasquezP.pdf"
+            href="/CV_HV_DavidFVasquezP.pdf"
             download
-            className="inline-flex items-center px-6 py-3 bg-black hover:scale-110 text-white rounded-lg font-semibold transition-transform transform hover:scale-105"
             target="_blank"
             rel="noopener noreferrer"
+            className="inline-flex items-center px-6 py-3 bg-black text-white rounded-lg font-semibold transition-transform hover:scale-105"
           >
             <svg
               className="w-5 h-5 mr-2"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v12m0 0l-4-4m4 4 4-4M4 20h16" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                d="M12 4v12m0 0l-4-4m4 4 4-4M4 20h16"
+              />
             </svg>
             Descargar Hoja de Vida
           </a>
